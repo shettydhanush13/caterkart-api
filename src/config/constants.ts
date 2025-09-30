86783a1d6ffce7077b139b8bf62d9650e8f95a2d
Dhanush.shetty <dhanush.shetty@cimpress.com>
2025-09-30 16:47:08 +0530

    REFACTOR

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,8 +1,8 @@
 export default {
     twillioConfig : {
-        accountSid : 'ACc53cabc6f6f3e106ff45a0c7aa48c843',
-        authToken : 'c491f3699d062c933cadca28b45e1b4a',
-        servicesId : 'VAbf4cac113c941e7c230092cbecc23af4',
-        recoveryCode: 'TPGTSTST8DUFQQN36PRFQMTM'
+        accountSid : process.env.TWILLIO_ACCOUNT_SID,
+        authToken : process.env.TWILLIO_AUTH_TOKEN,
+        servicesId : process.env.TWILLIO_SERVICES_ID,
+        recoveryCode: process.env.TWILLIO_RECOVERY_ID 
     },
 }
